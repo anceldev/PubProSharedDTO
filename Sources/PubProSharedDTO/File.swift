2d327efbe60a9d9ac1c6c9ad0df8d6c620fd0851
Ancel Guarayo <aguarayo@gmail.com>
2025-01-03 21:58:56 +0100

    Added ItemType

---
+++ Sources/PubProSharedDTO/File.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  PubProSharedDTO
+//
+//  Created by Ancel Dev account on 3/1/25.
+//
+
+import Foundation
+
+enum ItemType: String, Codable {
+    case consumption
+    case reward
+}
