851bbc0d33eb78ea825a30eaf56f93701e0f34d1
Ancel Guarayo <aguarayo@gmail.com>
2025-01-09 14:28:46 +0100

    Adding user in SignInResponseDTO

--- Sources/PubProSharedDTO/SignInResponseDTO.swift
+++ Sources/PubProSharedDTO/SignInResponseDTO.swift
@@ -12,12 +12,13 @@ public struct SignInResponseDTO: Codable, @unchecked Sendable {
     public let error: Bool
     public var reason: String? = nil
     public var token: String? = nil
-    public var userId: UUID? = nil
+//    public var userId: UUID? = nil
+    public var user: UserResponseDTO
     
-    public init(error: Bool, reason: String? = nil, token: String? = nil, userId: UUID? = nil) {
+    public init(error: Bool, reason: String? = nil, token: String? = nil, user: UserResponseDTO) {
         self.error = error
         self.reason = reason
         self.token = token
-        self.userId = userId
+        self.user = user
     }
 }
