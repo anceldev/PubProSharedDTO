9abdf2e50963a7d8da8edaffb06d29c49c95bf59
Ancel Guarayo <aguarayo@gmail.com>
2025-01-09 14:20:53 +0100

    SignInResponseDTO fixed

--- Sources/PubProSharedDTO/SignInResponseDTO.swift
+++ Sources/PubProSharedDTO/SignInResponseDTO.swift
@@ -7,26 +7,17 @@
 
 import Foundation
 
-public struct UUser: Codable {
-    public let id: UUID
-    public let username: String
-    public let email: String
-    public let points: Int
-    public let role: UserRole
-}
 
 public struct SignInResponseDTO: Codable, @unchecked Sendable {
     public let error: Bool
     public var reason: String? = nil
     public var token: String? = nil
     public var userId: UUID? = nil
-    public var userResponseDTO: UUser
     
-    public init(error: Bool, reason: String? = nil, token: String? = nil, userId: UUID? = nil, userResponseDTO: UUser) {
+    public init(error: Bool, reason: String? = nil, token: String? = nil, userId: UUID? = nil) {
         self.error = error
         self.reason = reason
         self.token = token
         self.userId = userId
-        self.userResponseDTO = userResponseDTO
     }
 }
